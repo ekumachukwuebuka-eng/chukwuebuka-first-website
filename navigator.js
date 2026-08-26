ad25878d3b0059ef92a66eb13260b2227f084c98
Ekuma Chukwuebuka Joseph <ekumachukwuebuka@gmail.com>
2026-08-26 21:02:46 +0100

    Refactor DOMContentLoaded event listener setup

--- navigator.js
+++ navigator.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const homePage = "/chukwuebuka-first-website/";
 
     // Handle all elements with the class "back-home"
-    const backButtons = document.querySelectorAll(".back-home");
+    const backButtons = document.querySelectorAll(");
 
     backButtons.forEach(function (button) {
         button.addEventListener("click", function (event) {
