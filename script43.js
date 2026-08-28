8642f15368be9bdccd52e94787f4d9bcd55229c4
Ekuma Chukwuebuka Joseph <ekumachukwuebuka@gmail.com>
2026-08-28 23:29:04 +0100

    Refactor password generation and slideshow functions

--- script43.js
+++ script43.js
@@ -29,3 +29,41 @@ function copyPassword() {
 
     alert("Password copied to clipboard!");
 }
+
+let currentSlide = 0;
+
+const banners = document.querySelectorAll(".moving-banner");
+
+function showSlide(index) {
+    if (index >= banners.length) {
+        currentSlide = 0;
+    }
+
+    if (index < 0) {
+        currentSlide = banners.length - 1;
+    }
+
+    banners.forEach((banner) => {
+        banner.style.display = "none";
+    });
+
+    banners[currentSlide].style.display = "block";
+}
+
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+}
+
+// Show the first image
+showSlide(currentSlide);
+
+
+
+
+
